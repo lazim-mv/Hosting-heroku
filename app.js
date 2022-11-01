10a065d4bd3ea8ef951d9d8fe4e563d2435bfd39
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2022-11-01 09:39:41 +0530

    First API Commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+    
+    let data = {
+        members: [
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    };
+
+    let jsonData = JSON.stringify(data);
+
+    const url = "https://us9.api.mailchimp.com/3.0/lists/40ba0e30f8"
+
+    const options = {
+        method:"POST",
+        auth: "lazim::96cbc1816223a9b0bc540ca3583a266c-us9"
+    }
+
+
+    const request = https.request(url,options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/success",function(req,res){
+    res.redirect("/");
+})
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server is running on port 3000.");
+});
+
+
+
